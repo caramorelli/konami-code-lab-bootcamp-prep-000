bbe493539e29e91df53802271740a107d2ca6673
Cara Morelli <caramorelli7@gmail.com>
2018-09-20 15:38:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,24 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+ 
+  var index = 0;
+
+  document.body.addEventListener('keydown',function(e){
+      var key = parseInt(e.which || e.detail);
+      const keyName = event.key
+      
+      if( key === codes[index] ){
+      index++;
+      
+        if(index === codes.length){
+        alert("Konami");
+        index = 0;
+      }
+    }
+    else{
+      console.log(e.which);
+      index = 0;
+    }
+  })
 }
